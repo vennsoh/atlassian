34b8fb9ecfce0e433be7449f2ed9947774f10c41
Ee Venn Soh <esoh@atlassian.com>
2019-11-28 11:45:56 +1100

    Update Checkbox.tsx
    Update Checkbox to include indeterminate state

--- atlassian-checkbox.framerfx/code/Checkbox.tsx
+++ atlassian-checkbox.framerfx/code/Checkbox.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import { addPropertyControls, ControlType } from "framer";
-import "@atlaskit/css-reset";
-import { CheckboxField } from "@atlaskit/form";
-import { Checkbox as AKCheckbox } from "@atlaskit/checkbox";
-import styled from "styled-components";
+import * as React from "react"
+import { addPropertyControls, ControlType } from "framer"
+import "@atlaskit/css-reset"
+import { CheckboxField } from "@atlaskit/form"
+import { Checkbox as AKCheckbox } from "@atlaskit/checkbox"
+import styled from "styled-components"
 
 const Wrapper = styled.div`
   > div {
@@ -14,71 +14,77 @@ const Wrapper = styled.div`
   label {
     cursor: pointer;
   }
-`;
+`
 
 Checkbox.defaultProps = {
-  width: 240,
-  height: 25,
-  label: "Checkbox",
-  isRequired: true,
-  isChecked: false,
-  isDisabled: false,
-  isInvalid: false,
-  onValueChange: () => null
-};
+    width: 240,
+    height: 25,
+    label: "Checkbox",
+    isRequired: true,
+    isChecked: false,
+    isDisabled: false,
+    isInvalid: false,
+    isIndeterminate: false,
+    onValueChange: () => null,
+}
 
 export function Checkbox(props) {
-  const [checkboxValue, setCheckboxValue] = React.useState(props.isChecked);
+    const [checkboxValue, setCheckboxValue] = React.useState(props.isChecked)
 
-  React.useEffect(() => {
-    if (checkboxValue !== props.isChecked) {
-      setCheckboxValue(props.isChecked);
+    React.useEffect(() => {
+        if (checkboxValue !== props.isChecked) {
+            setCheckboxValue(props.isChecked)
+        }
+    }, [props.isChecked])
+
+    const flipCheck = () => {
+        props.onValueChange(!checkboxValue)
+        setCheckboxValue(!checkboxValue)
     }
-  }, [props.isChecked]);
 
-  const flipCheck = () => {
-    props.onValueChange(!checkboxValue);
-    setCheckboxValue(!checkboxValue);
-  };
-
-  return (
-    <Wrapper>
-      <CheckboxField name="checkbox" isRequired={props.isRequired}>
-        {({ fieldProps }) => (
-          <AKCheckbox
-            {...fieldProps}
-            label={props.label}
-            isChecked={checkboxValue}
-            isDisabled={props.isDisabled}
-            isInvalid={props.isInvalid}
-            onChange={flipCheck}
-          />
-        )}
-      </CheckboxField>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <CheckboxField name="checkbox" isRequired={props.isRequired}>
+                {({ fieldProps }) => (
+                    <AKCheckbox
+                        {...fieldProps}
+                        label={props.label}
+                        isChecked={checkboxValue}
+                        isDisabled={props.isDisabled}
+                        isInvalid={props.isInvalid}
+                        isIndeterminate={props.isIndeterminate}
+                        onChange={flipCheck}
+                    />
+                )}
+            </CheckboxField>
+        </Wrapper>
+    )
 }
 
 addPropertyControls(Checkbox, {
-  label: {
-    type: ControlType.String,
-    title: "Label"
-  },
-  isChecked: {
-    type: ControlType.Boolean,
-    title: "Checked?",
-    defaultValue: false
-  },
-  isDisabled: {
-    type: ControlType.Boolean,
-    title: "Disabled?"
-  },
-  isInvalid: {
-    type: ControlType.Boolean,
-    title: "Invalid?"
-  },
-  isRequired: {
-    type: ControlType.Boolean,
-    title: "Required?"
-  }
-});
+    label: {
+        type: ControlType.String,
+        title: "Label",
+    },
+    isChecked: {
+        type: ControlType.Boolean,
+        title: "Checked?",
+        defaultValue: false,
+    },
+    isDisabled: {
+        type: ControlType.Boolean,
+        title: "Disabled?",
+    },
+    isInvalid: {
+        type: ControlType.Boolean,
+        title: "Invalid?",
+    },
+    isIndeterminate: {
+        type: ControlType.Boolean,
+        title: "Indeterminate?",
+    },
+    isRequired: {
+        type: ControlType.Boolean,
+        title: "Required?",
+    },
+})
